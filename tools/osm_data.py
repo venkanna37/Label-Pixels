c72a35585e8489683cc27b2a49d6bdba7e440f46
venkanna37 <g.venkanna37@gmail.com>
2021-04-11 15:33:44 +0530

    New script to download OpenStreetMap data

---
+++ tools/osm_data.py
@@ -0,0 +1,34 @@
+"""
+Script for downloading OpenStreetMap data to train deep learning models
+the data can be downloaded using bbox coordinates and bbox coordinates for vector or raster files
+Testing:
+Example command: python osm_data.py --bbox 30.33622 30.34384 78.03883 78.05268 --tags buildings
+ --outfile ../data/dd_buildings.geojson
+"""
+import osmnx as ox
+import argparse
+
+
+def download_osm(args):
+    bbox_coord = tuple(args.bbox)
+    filename = args.outfile
+    # test  coordinates: north, south, east, west = 30.33622 ,30.34384,78.03883,78.05268
+    north, south,east, west = bbox_coord[0], bbox_coord[1], bbox_coord[2], bbox_coord[3],
+    if args.tags == "roads":
+        tags = {'highway':'*'}
+    elif args.tags == "buildings":
+        tags = {'building: *'}
+    gdf = ox.geometries_from_bbox(north, south, east, west, tags)
+    gdf = gdf.apply(lambda c: c.astype(str) if c.name != 'geometry' else c, axis=0)
+    gdf.to_file(filename, driver='GeoJSON')
+    print("Data downloaded and saved in " + filename)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bbox", nargs='+', type=int, help="Bounding box coordinates, (north, south, east, west)")
+    parser.add_argument("--batch_size", type=int, help="Batch size", default=4)
+    parser.add_argument("--tags", type=str, help="Feature types ex: buildings, roads")
+    parser.add_argument("--outfile", type=str, help="Output filename with directory")
+    args = parser.parse_args()
+    download_osm(args)
