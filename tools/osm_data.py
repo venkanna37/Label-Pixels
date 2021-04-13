9de5ae11dda6fdfdd864240402190adaae4424ae
venkanna37 <g.venkanna37@gmail.com>
2021-04-13 17:13:57 +0530

    updated osm_data script

--- tools/osm_data.py
+++ tools/osm_data.py
@@ -2,8 +2,11 @@
 Script for downloading OpenStreetMap data to train deep learning models
 the data can be downloaded using bbox coordinates and bbox coordinates for vector or raster files
 Testing:
+osmnx library has to install
 Example command: python osm_data.py --bbox 30.33622 30.34384 78.03883 78.05268 --tags buildings
- --outfile ../data/dd_buildings.geojson
+ --outfile ../data/dd_roads.geojson
+
+The data has to clip with bounding box after downloading
 """
 import osmnx as ox
 import argparse
@@ -13,11 +16,12 @@ def download_osm(args):
     bbox_coord = tuple(args.bbox)
     filename = args.outfile
     # test  coordinates: north, south, east, west = 30.33622 ,30.34384,78.03883,78.05268
-    north, south,east, west = bbox_coord[0], bbox_coord[1], bbox_coord[2], bbox_coord[3],
+    north, south, east, west = bbox_coord[0], bbox_coord[1], bbox_coord[2], bbox_coord[3]
+    print("Downloading: " + args.tags)
     if args.tags == "roads":
-        tags = {'highway':'*'}
+        tags = {'highway': True}
     elif args.tags == "buildings":
-        tags = {'building: *'}
+        tags = {'building': True}
     gdf = ox.geometries_from_bbox(north, south, east, west, tags)
     gdf = gdf.apply(lambda c: c.astype(str) if c.name != 'geometry' else c, axis=0)
     gdf.to_file(filename, driver='GeoJSON')
@@ -26,7 +30,7 @@ def download_osm(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--bbox", nargs='+', type=int, help="Bounding box coordinates, (north, south, east, west)")
+    parser.add_argument("--bbox", nargs='+', type=float, help="Bounding box coordinates, (north, south, east, west)")
     parser.add_argument("--batch_size", type=int, help="Batch size", default=4)
     parser.add_argument("--tags", type=str, help="Feature types ex: buildings, roads")
     parser.add_argument("--outfile", type=str, help="Output filename with directory")
